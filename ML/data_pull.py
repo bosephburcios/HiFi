6cdf9f78fd1f72c2e3e2f4b888fc2acdf0f56ffe
alexy <alexyoonw@gmail.com>
2023-10-22 01:27:00 -0500

    fff

--- ML/data_pull.py
+++ ML/data_pull.py
@@ -11,13 +11,17 @@ zip_codes = df['ZCTA'].to_numpy().tolist()
 row = zip_codes.index(int(zip))
 categories = ('null', 'urban', 'suburban', 'rural')
 
+# urban, suburban, rural classification of the zip code
 category = categories[df['classification'].to_numpy()[row]]
 
 APIkey = "46e89de8832f4f74921204441232110"
 base = "http://api.weatherapi.com/v1"
 
 weather_info = requests.get(f"{base}/current.json?key={APIkey}&q={zip}&aqi=yes").json()
-current_weather = weather_info['current']['text']
+
+print(weather_info)
+
+# current_weather = weather_info['current']['text']
 
 # with open('data.json', 'w') as file:
     # json.dump(current_weather, file)
