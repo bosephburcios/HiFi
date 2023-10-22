4c142588165d90a7dfdc50586b9f37733597b407
alexy <alexyoonw@gmail.com>
2023-10-22 00:37:47 -0500

    f

--- ML/data_pull.py
+++ ML/data_pull.py
@@ -1,16 +1,23 @@
 import requests
 import json
+import numpy as nd
+import pandas as pd
 
 base = "http://ip-api.com/json/"
 
-city = requests.get(f"{base}").json()['city']
+zip = requests.get(f"{base}").json()['zip']
 
+df = pd.ExcelFile('urban-data.xlsx').parse(0)
+row = df['ZCTA'].to_numpy().tolist().index(int(zip))
+categories = ('null', 'urban', 'suburban', 'rural')
+
+category = categories[df['classification'].to_numpy()[row]]
+
+print(category)
 APIkey = "46e89de8832f4f74921204441232110"
 base = "http://api.weatherapi.com/v1"
 
-current_weather = requests.get(f"{base}/current.json?key={APIkey}&q={city}&aqi=yes").json()
-
-print(current_weather)
+current_weather = requests.get(f"{base}/current.json?key={APIkey}&q={zip}&aqi=yes").json()
 
 # with open('data.json', 'w') as file:
     # json.dump(current_weather, file)
--- ML/ip-api.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-import json
-
-base = "http://ip-api.com/json/"
-
-info = requests.get(f"{base}").json()
-
-print(info)
