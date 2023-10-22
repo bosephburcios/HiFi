e3d2c8a233500dddd9655baa79281fe761941b95
Joseph Turcios <joseph.angel349@gmail.com>
2023-10-22 06:59:24 -0500

    service

--- Front-End/option.js
+++ Front-End/option.js
@@ -1,4 +1,33 @@
-function toggleService(service) {
-    const details = service.querySelector(".service-details");
-    details.style.display = details.style.display === "block" ? "none" : "block";
-}
+// Function to toggle service details on small screens
+const serviceTitles = document.querySelectorAll('.service-title');
+
+serviceTitles.forEach(function (title) {
+    title.addEventListener('click', function () {
+        var service = title.closest('.service');
+        service.classList.toggle('active');
+
+        // Hide other service details
+        document.querySelectorAll('.service').forEach(function (otherService) {
+            if (otherService !== service) {
+                otherService.classList.remove('active');
+            }
+        });
+    });
+});
+
+// Function to handle the button click to show service details on small screens
+const serviceButtons = document.querySelectorAll('.get-started-button');
+
+serviceButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var service = button.closest('.service');
+        service.classList.toggle('active');
+
+        // Hide other service details
+        document.querySelectorAll('.service').forEach(function (otherService) {
+            if (otherService !== service) {
+                otherService.classList.remove('active');
+            }
+        });
+    });
+});
